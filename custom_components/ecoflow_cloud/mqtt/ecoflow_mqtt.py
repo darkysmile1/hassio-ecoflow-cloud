321131333cf13427b585217603f45aeaded1c75c
DarkY <bg_off@gmx.de>
2023-07-01 16:23:12 +0200

    type

--- custom_components/ecoflow_cloud/mqtt/ecoflow_mqtt.py
+++ custom_components/ecoflow_cloud/mqtt/ecoflow_mqtt.py
@@ -248,7 +248,7 @@ class EcoflowMQTTClient:
 
     def on_message(self, client, userdata, message):
         try:
-            if self.device_type == EcoflowModel.POWERSTREAM:
+            if self.device_type == 9:
                 raw = Serial.parse_powerstream_heartbeat(message.payload)
                 _LOGGER.error(f"Versuche extra zu decodieren")
             else:
